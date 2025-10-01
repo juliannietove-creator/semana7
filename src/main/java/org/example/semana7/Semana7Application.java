c966eaa535ceb5d822cfa380595b9093aefa6f38
Julian Nieto <julian_nietove@fet.edu.co>
2025-10-01 12:14:40 -0500

    primer commit

---
+++ src/main/java/org/example/semana7/Semana7Application.java
@@ -0,0 +1,13 @@
+package org.example.semana7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semana7Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/semana7/Semana7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.semana7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
